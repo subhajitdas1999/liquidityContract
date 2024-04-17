c5c2f2ac6dc80e6c69cc729369699836debc473c
subhajitdas1999 <sd651846@gmail.com>
2024-04-17 22:46:26 +0530

    formated code,removed only owner in mint func

--- contracts/MyERC20Token.sol
+++ contracts/MyERC20Token.sol
@@ -5,26 +5,24 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-contract MyERC20Token is ERC20,Ownable{
-
-    constructor(string memory _name,string memory _symbol,uint _initialSupply) ERC20(_name,_symbol){
-        _mint(owner(),_initialSupply*(10**(decimals())));
-    }
-    //mint tokensBits
-    function mint(uint _tokenSupply) public onlyOwner{
-        _mint(owner(),_tokenSupply);
-    }
-    //burn Token Bits
-    function burn(uint _tokenAmount) public onlyOwner{
-        _burn(owner(),_tokenAmount);
+contract MyERC20Token is ERC20, Ownable {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint _initialSupply
+    ) ERC20(_name, _symbol) {
+        _mint(owner(), _initialSupply * (10 ** (decimals())));
     }
 
-    function decimals() public pure  override returns (uint8) {
-        return 0;
+    function mint(uint _amount) public {
+        _mint(msg.sender, _amount * (10 ** (decimals())));
     }
 
-    
+    function burn(uint _tokenAmount) public onlyOwner {
+        _burn(owner(), _tokenAmount);
+    }
 
-    
+    function decimals() public pure override returns (uint8) {
+        return 9;
+    }
 }
